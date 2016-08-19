38067a0378cdaa6d9854419afaf8091d0ebd832c
Mike Dame <mdame@redhat.com>
2016-08-19 14:18:59 -0400

    Initial commit

---
+++ cmd/controller/cmd.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	_ "github.com/openshift/origin/pkg/api/install"
+	osclient "github.com/openshift/origin/pkg/client"
+	kclient "k8s.io/kubernetes/pkg/client/unversioned"
+)
