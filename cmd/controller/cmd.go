e13490ff015e2953052a45a02578feba5f6c186a
Mike Dame <mdame@redhat.com>
2016-08-22 11:43:49 -0400

    update

--- cmd/controller/cmd.go
+++ cmd/controller/cmd.go
@@ -15,15 +15,13 @@ import (
 )
 
 func main() {
-	var openshiftClient osclient.Interface
 	config, err := clientcmd.DefaultClientConfig(pflag.NewFlagSet("empty", pflag.ContinueOnError)).ClientConfig()
-	oc, err := osclient.New(config)
 	kubeClient, err := kclient.New(config)
 	if err != nil {
 		log.Printf("Error creating cluster config: %s", err)
 		os.Exit(1)
 	}
-	openshiftClient, err = osclient.New(config)
+	openshiftClient, err := osclient.New(config)
 	if err != nil {
 		log.Printf("Error creating OpenShift client: %s", err)
 		os.Exit(2)
