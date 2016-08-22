ac5131c556a212a3abce6b084d70b3ad4b7a540f
Mike Dame <mdame@redhat.com>
2016-08-22 16:53:58 -0400

    kclient

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,29 +4,54 @@ import (
 	"fmt"
 
 	osclient "github.com/openshift/origin/pkg/client"
+	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
 
 	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/meta"
 	kclient "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
+	"k8s.io/kubernetes/pkg/runtime"
 )
 
 type Controller struct {
 	openshiftClient *osclient.Client
 	kubeClient      *kclient.Client
+	mapper          meta.RESTMapper
+	typer           runtime.ObjectTyper
+	f               *clientcmd.Factory
 }
 
 func NewController(os *osclient.Client, kc *kclient.Client) *Controller {
+
+	f := clientcmd.New(pflag.NewFlagSet("empty", pflag.ContinueOnError))
+	mapper, typer := f.Object()
+
 	return &Controller{
 		openshiftClient: os,
 		kubeClient:      kc,
+		mapper:          mapper,
+		typer:           typer,
+		f:               f,
 	}
 }
 
 func (c *Controller) Run() {
-	projects, err := c.openshiftClient.Projects().List(kapi.ListOptions{})
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, project := range projects.Items {
-		fmt.Printf("%s\n", project.ObjectMeta.Name)
-	}
+	/*
+		// Old code using OpenShift
+		projects, err := c.openshiftClient.Projects().List(kapi.ListOptions{})
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, project := range projects.Items {
+			fmt.Printf("%s\n", project.ObjectMeta.Name)
+		}
+	*/
+
+	r := resource.NewBuilder(c.mapper, c.typer, resource.ClientMapperFunc(c.f.ClientForMapping), kapi.Codecs.UniversalDecoder()).
+		ResourceTypeOrNameArgs(true, "projects").
+		Flatten().
+		Do()
+	err := r.Visit(func(info *resource.Info, err error) error {
+		fmt.Printf("%s\n", info.Name)
+	})
 }
